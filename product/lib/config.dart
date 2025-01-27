4f773eed2111b85aa51f012533a7ae2e1f77011c
Ikuyaz <zhenseng01@gmail.com>
2025-01-27 18:09:02 +0800

    organize and update

---
+++ product/lib/config.dart
@@ -0,0 +1,9 @@
+class Config {
+  static const String sqlProductsUrl =
+      'http://192.168.0.73:5000/api/stock_items/company/2';
+  static const String platformProductsUrl =
+      'http://192.168.0.240:7000/products';
+  static const String mapProductsUrl = 'http://192.168.0.73:5000/api/products';
+  static const int storeId = 2;
+  static const String currency = 'MYR';
+}
---
+++ product/lib/link_product.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'config.dart';
+import 'services.dart';
+import 'sql_product.dart';
+import 'platform_product.dart';
+import 'MappedProductsPage.dart';
+
+class LinkProductPage extends StatefulWidget {
+  @override
+  _LinkProductPageState createState() => _LinkProductPageState();
+}
+
+class _LinkProductPageState extends State<LinkProductPage> {
+  List<dynamic> sqlProducts = [];
+  List<dynamic> platformProducts = [];
+  dynamic selectedSQLProduct;
+  Set<dynamic> selectedPlatformProducts = {};
+  List<dynamic> filteredProducts = [];
+  String searchQuery = '';
+
+  @override
+  void initState() {
+    super.initState();
+    filteredProducts = platformProducts;
+  }
+
+  void updateSearchQuery(String query) {
+    setState(() {
+      searchQuery = query;
+      filteredProducts = platformProducts.where((product) {
+        final skus = product['skus'] as List<dynamic>;
+        return skus.any((sku) =>
+            sku['ShopSku'].toLowerCase().contains(query.toLowerCase()));
+      }).toList();
+    });
+  }
+
+  Future<void> fetchSQLProducts() async {
+    try {
+      final products = await ApiService.fetchSQLProducts(Config.sqlProductsUrl);
+      setState(() {
+        sqlProducts = products;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> fetchPlatformProducts() async {
+    try {
+      final products =
+          await ApiService.fetchPlatformProducts(Config.platformProductsUrl);
+      setState(() {
+        platformProducts = products;
+        filteredProducts = platformProducts;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Link Product')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text('Company ID: 10002', style: TextStyle(fontSize: 16)),
+            SizedBox(height: 20),
+            Expanded(
+              child: Row(
+                children: [
+                  SQLProductList(
+                    products: sqlProducts,
+                    onFetch: fetchSQLProducts,
+                    selectedProduct: selectedSQLProduct,
+                    onSelect: (product) => setState(() {
+                      selectedSQLProduct = product;
+                    }),
+                  ),
+                  VerticalDivider(),
+                  PlatformProductList(
+                    products: filteredProducts,
+                    onFetch: fetchPlatformProducts,
+                    selectedProducts: selectedPlatformProducts,
+                    onSelect: (sku) => setState(() {
+                      if (selectedPlatformProducts.contains(sku)) {
+                        selectedPlatformProducts.remove(sku);
+                      } else {
+                        selectedPlatformProducts.add(sku);
+                      }
+                    }),
+                    onSearch: updateSearchQuery,
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (selectedSQLProduct != null) {
+                  List<Map<String, dynamic>> products =
+                      selectedPlatformProducts.map((sku) {
+                    return {
+                      'stock_item_id': selectedSQLProduct['id'],
+                      'price': sku['price'],
+                      'discounted_price': sku['special_price'],
+                      'sku': sku['ShopSku'],
+                      'currency': Config.currency,
+                      'status': sku['Status'],
+                    };
+                  }).toList();
+
+                  Map<String, dynamic> requestBody = {
+                    'store_id': Config.storeId,
+                    'products': products,
+                  };
+
+                  try {
+                    await ApiService.mapProducts(
+                        Config.mapProductsUrl, requestBody);
+                    print('Products mapped successfully');
+                  } catch (e) {
+                    print(e);
+                  }
+                }
+              },
+              child: Text('Map'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        MappedProductsPage(storeId: Config.storeId),
+                  ),
+                );
+              },
+              child: Text('View Mapped Products'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
